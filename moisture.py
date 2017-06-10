505158d193507f091dd027a375b7cc7171486ad6
Jakub Horak <horak@zee.cz>
2017-06-10 17:51:26 +0200

    Fix script

--- moisture.py
+++ moisture.py
@@ -4,6 +4,7 @@
 
 import RPi.GPIO as GPIO # This is the GPIO library we need to use the GPIO pins on the Raspberry Pi
 import urllib2 # To send GET request
+import time
 import datetime
 
 # Define some variables to be used later on in our script
@@ -14,20 +15,22 @@ webhook_url = "https://api.thingspeak.com/update?api_key=3YJ24WQGWH6XG358"
 
 def submitValue(value):
     try:
-        response = urllib2.urlopen(webhook_url + "&field1=" + str(value))
-        print "Successfully submitted value. Response:", response.read() 
-    except urllib2.URLError:
-        print "Error: unable to submit value: " + webhook_url + "&field1=" + str(value)
+        url = webhook_url + "&field1=" + str(value)
+        response = urllib2.urlopen(url)
+        print "Successfully submitted value. Response: {0}".format(response.read())
+    except urllib2.URLError as e:
+        print "Error: unable to submit value to URL: {0}, {1}".format(url, e)
 
 # This is our callback function, this function will be called every time there is a change on the specified GPIO channel, in this example we are using 17
 
 def callback(channel):  
-	if GPIO.input(channel):
-		print "LED off"
-		submitValue(1)
-	else:
-		print "LED on"
-		submitValue(0)
+    print datetime.datetime.now()
+    if GPIO.input(channel):
+        print "LED off"
+        submitValue(1)
+    else:
+        print "LED on"
+        submitValue(0)
 
 # Set our GPIO numbering to BCM
 GPIO.setmode(GPIO.BCM)
@@ -37,5 +40,4 @@ channel = 17
 # Set the GPIO pin to an input
 GPIO.setup(channel, GPIO.IN)
 
-print datetime.datetime.now()
 callback(channel)
